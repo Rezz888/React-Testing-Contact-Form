cea3ac7eca6d152726f62a2e58421dbfbf26d041
Rezaur Rahman <rezaur.rahman_nyc@icloud.com>
2021-01-04 01:52:48 -0500

    started writing some intro test

---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ContactForm from './ContactForm';
+import { render } from '@testing-library/react';
+
+test("render this component", () => {
+    render(< ContactForm />);
+})
